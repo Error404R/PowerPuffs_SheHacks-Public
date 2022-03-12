7417b77d509ed83cce87d3c8d40bba54fc76f401
Raunak Rathore <64827299+Error404R@users.noreply.github.com>
2022-03-13 01:11:40 +0530

    Add files via upload

---
+++ PowerPuffs_SheHacks-main/jobSearch/app.js
@@ -0,0 +1,6 @@
+
+import { JobSearch } from './JobSearch';
+
+const jobSearch = new JobSearch('#search-form', '.result-container', '.loading-element');
+jobSearch.setCountryCode();
+jobSearch.configureFormListener();
